b9fa34795fb7919fb76f232b391acc4efe8b0467
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2025-05-25 11:29:22 -0500

    fix: estructura inicial + configuración de BD y conexión a PostgreSQL y soporte Liquibase agregados

---
+++ src/main/resources/db/changelog/V1.0/migrations/001_archivo_prueba_sql.sql
@@ -0,0 +1,6 @@
+--liquibase formatted sql
+
+--changeset santiago.quintero:V1.001.0 splitStatements:false runOnChange:true
+    --Contenido
+-- Esta es un cometario para la estructura de liquibase
+    --Rollback
